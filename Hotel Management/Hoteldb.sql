908a75d6acff52841e0d75db93cca06f5b1cedce
SaubhagyaSingh <saubhagyasingh65@gmail.com>
2021-01-05 01:12:58 +0530

    Initial commit

---
+++ Hotel Management/Hoteldb.sql
@@ -0,0 +1,4 @@
+use hoteldatabase;
+create table Data (Customer_id int,First_Name varchar(20),Surname varchar(20),Address varchar(90),Postcode int,Mobile_No int,Email varchar(40),Gender varchar(15),Idproof varchar(40),Room_No int,DOB date,check_in_date date, check_out_Date date,Room_Type varchar(40));
+show tables;
+describe data;
